e67828abbaafade999258025083b98690ed4e02b
Thomas Segismont <tsegismo@redhat.com>
2016-07-01 19:21:52 +0200

    HWKMETRICS-421 Hawkular Metrics versioned support
    Check version on graph panel initialization and then decide if we can
    use the new POST method endpoints or fallback to a legacy call.
    
    Legacy call uses deprecated data GET method endpoints, and applies rate
    calculation on the client side. This is because old versions don't have
    the rate endpoint for gauges.

--- src/datasource.js
+++ src/datasource.js
@@ -10,60 +10,104 @@ export class GenericDatasource {
     this.token = instanceSettings.jsonData.token;
     this.q = $q;
     this.backendSrv = backendSrv;
+    this.queryFunc = this.selectQueryFunc();
   }
 
   query(options) {
-    var promises = _.chain(options.targets)
+    var targets = _.chain(options.targets)
       .filter(target => !target.hide)
       .filter(target => target.target !== 'select metric')
-      .map(target => {
-
-        var uri = [];
-        uri.push(target.type + 's'); // gauges or counters
-        uri.push(target.rate ? 'rate' : 'raw'); // raw or rate
-        uri.push('query');
-
-        var url = this.url + '/' + uri.join('/');
-
-        return this.backendSrv.datasourceRequest({
-          url: url,
-          data: {
-            ids: [target.target],
-            start: options.range.from.valueOf(),
-            end: options.range.to.valueOf()
-          },
-          method: 'POST',
-          headers: this.createHeaders()
-        }).then(response => {
-          return {
-            refId: target.refId,
-            target: target.target,
-            response: response
-          };
-        });
-      })
       .value();
 
-    if (promises.length <= 0) {
+    if (targets.length == 0) {
       return this.q.when({data: []});
     }
 
-    return this.q.all(promises).then(richResponses => {
-      var result = _.map(richResponses, (richResponse) => {
-        var response = richResponse.response;
-        var datapoints;
-        if (response.data.length != 0) {
-          datapoints = _.map(response.data[0].data, point => [point.value, point.timestamp]);
-        } else {
-          datapoints = [];
-        }
-        return {
-          refId: richResponse.refId,
-          target: richResponse.target,
-          datapoints: datapoints
-        };
-      });
+    var start = options.range.from.valueOf();
+    var end = options.range.to.valueOf();
+
+    var promises = _.map(targets, target => this.queryFunc.then(func => func.call(this, target, start, end)));
+
+    return this.q.all(promises).then(result => {
       return {data: result};
+    });
+  }
+
+  getData(target, start, end) {
+    var uri = [];
+    uri.push(target.type + 's'); // gauges or counters
+    uri.push(target.rate ? 'rate' : 'raw'); // raw or rate
+    uri.push('query');
+
+    var url = this.url + '/' + uri.join('/');
+
+    return this.backendSrv.datasourceRequest({
+      url: url,
+      data: {
+        ids: [target.target],
+        start: start,
+        end: end
+      },
+      method: 'POST',
+      headers: this.createHeaders()
+    }).then(response => {
+      var datapoints;
+      if (response.data.length != 0) {
+        datapoints = _.map(response.data[0].data, point => [point.value, point.timestamp]);
+      } else {
+        datapoints = [];
+      }
+      return {
+        refId: target.refId,
+        target: target.target,
+        datapoints: datapoints
+      };
+    });
+  }
+
+  getDataLegacy(target, start, end) {
+    var uri = [];
+    uri.push(target.type + 's'); // gauges or counters
+    uri.push(encodeURIComponent(target.target).replace('+', '%20')); // metric name
+    uri.push('data');
+
+    var url = this.url + '/' + uri.join('/');
+
+    return this.backendSrv.datasourceRequest({
+      url: url,
+      params: {
+        start: start,
+        end: end
+      },
+      method: 'GET',
+      headers: this.createHeaders()
+    }).then(response => {
+      var datapoints;
+      if (!target.rate) {
+        datapoints = _.map(response.data, point => [point.value, point.timestamp]);
+      } else {
+        var sortedData = response.data.sort((p1, p2)=> p1.timestamp - p2.timestamp);
+        datapoints = _.chain(sortedData)
+          .zip(sortedData.slice(1))
+          .filter(pair => {
+            return pair[1] // Exclude the last pair
+              && (target.type == 'gauge' || pair[0].value <= pair[1].value); // Exclude counter resets
+          })
+          .map(pair => {
+            var point1 = pair[0], point2 = pair[1];
+            var timestamp = point2.timestamp;
+            var value_diff = point2.value - point1.value;
+            var time_diff = point2.timestamp - point1.timestamp;
+            var rate = 60000 * value_diff / time_diff;
+            return [rate, timestamp];
+          })
+          .value();
+      }
+      return {
+        refId: target.refId,
+        target: target.target,
+        datapoints: datapoints
+      };
     });
   }
 
@@ -110,5 +154,27 @@ export class GenericDatasource {
         return {text: metric.id, value: metric.id};
       });
     });
+  }
+
+  selectQueryFunc() {
+    return this.backendSrv.datasourceRequest({
+      url: this.url + '/status',
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'}
+    }).then(response => {
+      var version = response.data['Implementation-Version'];
+      var regExp = new RegExp('([0-9]+)\.([0-9]+)\.(.+)');
+      if (version.match(regExp)) {
+        var versionInfo = regExp.exec(version);
+        var major = versionInfo[1];
+        var minor = versionInfo[2];
+        if (major == 0) {
+          if (minor < 17) {
+            return this.getDataLegacy;
+          }
+        }
+      }
+      return this.getData;
+    }).catch(response => this.getData);
   }
 }
